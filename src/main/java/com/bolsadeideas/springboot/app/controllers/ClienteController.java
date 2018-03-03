ca2efb4848274e8229c275c56c8e3754b1e60068
Carlos Ariel <ariels7801@gmail.com>
2018-03-02 20:55:56 -0600

    24. Creando la vista crear y formulario

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -40,6 +40,7 @@ public class ClienteController {
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
     public String guardar(Cliente cliente){
+
         clienteDao.save(cliente);
         return "redirect:listar";
     }
