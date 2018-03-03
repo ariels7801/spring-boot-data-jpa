30b8f827d741c7abfa4c205e713861c5bf496113
Carlos Ariel <ariels7801@gmail.com>
2018-03-03 14:34:56 -0600

    29. Agregando editar (SessionAttributes)

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -7,13 +7,18 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
+// Se guarda en sesion los datos  del objeto cliente
+@SessionAttributes("cliente")
 public class ClienteController {
 
     // Con esta anotación va a buscar un componente que implemente esta interfase (busca un bean) //
@@ -30,7 +35,7 @@ public class ClienteController {
     }
 
     @RequestMapping(value = "/form")
-    public String crear(Map<String, Object> model){
+    public String crear(Map<String, Object> model) {
 
         Cliente cliente = new Cliente();
 
@@ -41,17 +46,34 @@ public class ClienteController {
     }
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
-    public String guardar(@Valid Cliente cliente, BindingResult result, Model model){
+    public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
 
-        if (result.hasErrors()){
+        if (result.hasErrors()) {
             model.addAttribute("titulo", "Formulario de Cliente");
             return "form";
         }
 
         clienteDao.save(cliente);
+        // Borra de la session los datos del objeto cliente
+        status.setComplete();
         return "redirect:listar";
     }
 
+    @RequestMapping(value = "/form/{id}", method = RequestMethod.GET)
+    public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model) {
+
+        Cliente cliente = null;
+
+        if (id > 0) {
+            cliente = clienteDao.findOne(id);
+        } else {
+            return "redirect:/listar";
+        }
+
+        model.put("cliente", cliente);
+        model.put("titulo", "Editar Cliente");
+        return "form";
+    }
 
 
 }
