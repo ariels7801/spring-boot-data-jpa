affe57791bea35b39d10917b5b12b0a71c8257ff
Carlos Ariel <ariels7801@gmail.com>
2018-03-04 13:36:43 -0600

    38. Bonus track: Flash Messenger

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.util.Map;
@@ -31,6 +32,27 @@ public class ClienteController {
         return "listar";
     }
 
+    @RequestMapping(value = "/form/{id}", method = RequestMethod.GET)
+    public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
+
+        Cliente cliente = null;
+
+        if (id > 0) {
+            cliente = clienteService.findOne(id);
+            if (cliente == null) {
+                flash.addFlashAttribute("error", "El Id del cliente no existe!");
+                return "redirect:/listar";
+            }
+        } else {
+            flash.addFlashAttribute("error", "El Id del cliente no puede ser cero!");
+            return "redirect:/listar";
+        }
+
+        model.put("cliente", cliente);
+        model.put("titulo", "Editar Cliente");
+        return "form";
+    }
+
     @RequestMapping(value = "/form")
     public String crear(Map<String, Object> model) {
 
@@ -43,39 +65,27 @@ public class ClienteController {
     }
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
-    public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
+    public String guardar(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash, SessionStatus status) {
 
         if (result.hasErrors()) {
             model.addAttribute("titulo", "Formulario de Cliente");
             return "form";
         }
 
+        String mensajeFlash = (cliente.getId() != null)? "Cliente editado con exíto!" : "Cliente creado con exíto!";
+
         clienteService.save(cliente);
         // Borra de la session los datos del objeto cliente
         status.setComplete();
+        flash.addFlashAttribute("success", mensajeFlash);
         return "redirect:listar";
     }
 
-    @RequestMapping(value = "/form/{id}", method = RequestMethod.GET)
-    public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model) {
-
-        Cliente cliente = null;
-
-        if (id > 0) {
-            cliente = clienteService.findOne(id);
-        } else {
-            return "redirect:/listar";
-        }
-
-        model.put("cliente", cliente);
-        model.put("titulo", "Editar Cliente");
-        return "form";
-    }
-
     @RequestMapping(value = "/eliminar/{id}")
-    public String eliminar(@PathVariable(value = "id") Long id){
+    public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash){
         if (id > 0) {
             clienteService.delete(id);
+            flash.addFlashAttribute("success", "Cliente eliminado con exíto!");
         }
         return "redirect:/listar";
     }
